7957a95639a44e161cb9e51817857fd1372ba4ad
Karanbir Singh <karanbirsingh1161@gmail.com>
2018-12-31 10:41:31 -0500

    Update routes

--- src/base.js
+++ src/base.js
@@ -2,9 +2,9 @@ import Rebase from 're-base';
 import firebase from 'firebase';
 
 const firebaseApp = firebase.initializeApp({
-    apiKey: "AIzaSyBI0MzRXJzSixPY-JYF4xU-yI2pJMNKaDA",
-    authDomain: "catch-of-the-day-singkar.firebaseapp.com",
-    databaseURL: "https://catch-of-the-day-singkar.firebaseio.com"
+    apiKey: "$KEY",
+    authDomain: "$DOMAIN",
+    databaseURL: "$DB_URL"
 });
 
 const base = Rebase.createClass(firebaseApp.database());
